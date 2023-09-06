5f56390ea286c6efc0b28bfe24c3535cd9d578b9
applesniperz <ngoseng_gee@live.com.sg>
2023-09-06 17:02:53 +0800

    Add files via upload
    Version 1

---
+++ Pie Charts/Pie Charts/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Pie Charts
+//
+//  Created by I3LT-040 on 6/9/23.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    @State private var pie = PieChartV2()
+
+    var body: some View {
+        ZStack {
+        pie.chart(
+            PlottableValue(name: "Rock", value: 90, color: .blue), // This is variadic call
+            PlottableValue(name: "Indie", value: 10, color: .pink),
+            PlottableValue(name: "Pop", value: 80, color: .mint)
+        )
+        .frame(height: 250)
+            // Overlay a circular picture
+            Image("CirclePicture") // Replace with your image asset name
+                .resizable()
+                .frame(width: 175, height: 175)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                .offset(x: 0, y: -250) // Adjust the offset as needed
+        
+        }
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pie Charts/Pie Charts/PieChartV2.swift
@@ -0,0 +1,123 @@
+//
+//  PieChart.swift
+//  Pie Charts
+//
+//  Created by I3LT-040 on 6/9/23.
+//
+
+
+import SwiftUI
+
+//struct PlottableValue: Identifiable {
+//    var id = UUID()
+//    var name: String
+//    var value: Double
+//    var color: Color
+//}
+
+class PieChartV2 {
+    // where to position the legend
+    enum Legend {
+        case top, bottom, leading, trailing, hidden
+    }
+
+    //draw an segment of the pie chart
+    private struct PieSegment: Shape, Identifiable {
+        var data: PlottableValue
+        var id: UUID { data.id }
+        var startAngle: Double
+        var amount: Double
+
+        func path(in rect: CGRect) -> Path {
+            let radius = min(rect.width, rect.height) * 0.50
+            let center = CGPoint(x: rect.width * 0.5, y: rect.height * -0.55)
+
+            var path = Path()
+            path.move(to: center)
+            path.addRelativeArc(center: center, radius: radius, startAngle: Angle(radians: startAngle), delta: Angle(radians: amount))
+            return path
+        }
+    }
+
+    /// A method to calculate the segment sizes
+    /// - Parameters:
+    ///   - values: PlottableValue as a variadic call to be able add as many as needed
+    ///   - legend: Where the position of legend is located (default on bottom)
+    /// - Returns: A View of the completed Pie Chart
+    func chart(_ values: PlottableValue..., legend: Legend = .bottom) -> some View {
+        let totalValue = values.reduce(0) { $0 + $1.value }
+        var startAngle = -Double.pi / 2
+
+        var pieSegments  = [PieSegment]()
+
+        for data in values {
+            let amount = .pi * 2 * (data.value / totalValue )
+            let segment = PieSegment(data: data, startAngle: startAngle, amount: amount)
+            pieSegments.append(segment)
+            startAngle += amount
+        }
+
+       // A View of the combined segments
+        var chartView: some View {
+            ZStack {
+                ForEach(pieSegments) { segment in
+                    segment
+                        .fill(segment.data.color)
+                }
+            }
+        }
+
+        // A View of the name of each segments
+        var legendView: some View {
+            ForEach(pieSegments) { segment in
+                Label {
+                    Text(segment.data.name)
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                } icon: {
+                    Circle()
+                        .fill(segment.data.color)
+                        .frame(width: 10)
+                }
+            }
+        }
+
+        // return View of completed switching on where chosen placement of legend
+        return Group {
+            switch legend {
+            case .bottom:
+                VStack {
+                    chartView
+                    HStack {
+                       legendView
+                    }
+                }
+            case .top:
+                VStack {
+                    HStack {
+                        legendView
+                    }
+                    chartView
+                }
+            case .leading:
+                HStack {
+                    VStack(alignment: .leading) {
+                       legendView
+                    }
+                    .padding(.leading)
+                   chartView
+                }
+            case .trailing:
+                HStack {
+                   chartView
+                    VStack(alignment: .leading) {
+                       legendView
+                    }
+                    .padding(.trailing)
+                }
+            case .hidden:
+                chartView
+            }
+        }
+    }
+}
